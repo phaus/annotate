de5513b834b9d9ed037f5e9631c046c2c96b6b8d
Philipp Haußleiter <philipp@haussleiter.de>
2012-05-12 01:23:57 +0200

    fixing some errors

--- public/js/bm-instapaper.js
+++ public/js/bm-instapaper.js
@@ -7,7 +7,7 @@ function iprl5(){
         if(!b)
             throw(0);
         d.title='(Saving...) '+d.title;
-        z.setAttribute('src',l.protocol+'//www.instapaper.com/j/Vo4rNAcQ4OCO?u='+encodeURIComponent(l.href)+'&amp;t='+(new Date().getTime()));
+        z.setAttribute('src',l.protocol+'//www.instapaper.com/j/foobar?u='+encodeURIComponent(l.href)+'&amp;t='+(new Date().getTime()));
         b.appendChild(z);
     }catch(e){
         alert('Please wait until the page has loaded.');
